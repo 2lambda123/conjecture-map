8b0670a04d1c11d444b6ca63cf4c6e7af8814e1a
Jacob Kelter <>
2021-06-02 16:09:35 -0700

    added a box for high level conjecture

---
+++ src/ConjectureMap.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css'
+import HighLevelConjecture from './HighLevelConjecture'
+
+function ConjectureMap() {
+    return (
+        <div>
+            <HighLevelConjecture />
+            {/* <Columns /> */}
+        </div>
+    )
+}
+
+export default ConjectureMap
---
+++ src/HighLevelConjecture.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function HighLevelConjecture() {
+    return (
+        <div id="high-level-conjecture">
+            High Level Conjecture: 
+            <div>
+                <textarea id="high-level-conjecture-input" />
+            </div>
+        </div>
+    )
+}
+
+export default HighLevelConjecture
